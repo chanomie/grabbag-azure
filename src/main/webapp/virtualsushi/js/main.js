7dec7ce78f5dcfdff1cf36a29c9a7bf887330502
Jordan Reed <2756092+chanomie@users.noreply.github.com>
2019-09-24 12:50:59 -0700

    Rolling out product pages

---
+++ src/main/webapp/virtualsushi/js/main.js
@@ -0,0 +1,16 @@
+//
+// This is the main JS file.  Virtual Sushi avoids server-side logic in favor of client-side
+// JavaScript for fun.
+
+
+// The Product Database
+var products = {
+  "VCR": {
+  	"name":"Virtual California Roll",
+  	"price":"4.99"
+  },
+  "VST": {
+  	"name":"Virtual Spicy Tuna",
+  	"price":"7.99"
+  }		
+};
